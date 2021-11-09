290c3fc0cfb1a907366fc9f5dd2b74686a8ceca3
joe-nomad <1921731@iub.edu.bd>
2021-11-10 00:13:22 +0600

    this is the 4th time im doing this

---
+++ articles/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Article
+class ArtilceAdmin(admin.ModelAdmin):
+    list_display= ['id','title','content','timestamp','updated']
+    search_field =['id','title']
+
+admin.site.register(Article,ArtilceAdmin)
---
+++ articles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    
