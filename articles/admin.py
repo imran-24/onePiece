786bc5e01b0a7e72158aa93a7686f2ecb072fc96
joe-nomad <1921731@iub.edu.bd>
2021-11-10 02:07:57 +0600

    before doing pre_save and post_save

--- articles/admin.py
+++ articles/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Article
 class ArtilceAdmin(admin.ModelAdmin):
-    list_display= ['id','title','content','timestamp','updated']
+    list_display= ['id','title','content','slug','timestamp','updated']
     search_field =['id','title']
 
 admin.site.register(Article,ArtilceAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify 
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(null=True,blank=True)
     content = models.TextField()
+    time = models.TimeField(default=timezone.now)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    
+    def save(self): # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save()
